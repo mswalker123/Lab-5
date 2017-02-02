59581e163bd14bd9161c62fa1c0068988151fc6f
Mishara Walker <mswalker123@gmail.com>
2017-02-02 14:59:56 -0500

    reversed the C to F

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ var temperatureInF= prompt ('Enter Temperature in Fahrenheit');
 var temperatureInC = (temperatureInF - 32) * 5/9;
 
 console.log('the temperature is ' + temperatureInC + 'C');
+
+var temperatureInC = prompt ('Enter Temperature in Celcius');
+var temperatureInF = (temperatureInC * 9/5) + 32 ;
+
+console.log('the temperature is ' + temperatureInF + 'F');
